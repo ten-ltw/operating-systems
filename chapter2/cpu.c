0e03ebb26880163fdc8eeafd1e73af800ee2f107
Ten Li <li.tw.ten@gmail.com>
2022-05-25 20:45:21 +0800

    first commit

---
+++ chapter2/cpu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
+#include "./common.h"
+
+int main(int argc, char *argv[])
+{
+  printf("%d\n", argc);
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: cpu <string>\n");
+    exit(1);
+  }
+  char *string = argv[1];
+  while (1)
+  {
+    Spin(1);
+    printf("%s\n", string);
+  }
+  return 0;
+}
---
+++ chapter2/cpu.md
@@ -0,0 +1,30 @@
+## gcc
+
+gcc 是编译器。
+make 是个工具，通过 makefile 调度 gcc 编译。
+
+```shell
+gcc -o cpu cpu.c -Wall
+```
+
+`-Wall` 选项意思是编译后显示所有警告。
+
+`-W` 选项类似 `-Wall`，会显示警告，但是只显示编译器认为会出现错误的警告。
+
+## include
+
+#include 通过相对路径引库
+
+## main 函数
+
+main 函数参数
+main (argc,argv)
+
+- argc (第一个形参)必须是整型变量
+- argv (第二个形参)必须是指向字符串的指针数组
+
+## exit 函数
+
+## fprintf
+
+## printf
